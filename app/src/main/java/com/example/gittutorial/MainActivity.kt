be2c046d02404373f76245aac945931dc8685c04
CamiloSanchez <camilo1632@gmail.com>
2023-08-11 20:43:55 -0500

    deleted prints

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -7,14 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("This is a message")
-        print("This is a message")
-        print("This is a message")
-        print("This is a message")
-        print("This is a message")
-        print("This is a message")
-        print("This is a message")
-        print("This is a message")
-        print("This is a message")
     }
 }
