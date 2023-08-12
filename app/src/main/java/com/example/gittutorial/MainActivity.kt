cd723196ecac91bdaf22ca85598a9eecf8d800bf
CamiloSanchez <camilo1632@gmail.com>
2023-08-11 20:42:17 -0500

    included print example 2

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("This is a message")
+        print("This is a message")
+        print("This is a message")
+        print("This is a message")
+        print("This is a message")
+        print("This is a message")
+        print("This is a message")
+        print("This is a message")
+        print("This is a message")
     }
 }
